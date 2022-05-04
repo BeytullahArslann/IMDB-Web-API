9d223e5ad10c5d26ad12a79faec793522aaa068d
Beytullah ARSLAN <beytullahars0@gmail.com>
2022-05-04 15:02:41 +0300

    Add project files.

---
+++ IMDB-Web-API/Controllers/IMDBMovieController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IMDB_Web_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IMDBMovieController : ControllerBase
+    {
+    }
+}
